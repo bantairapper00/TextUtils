a53b521ab59aa085b7b2d078f7f5fdbe1f6475fd
ABHISHEK KUMAR <abhikr1708@gmail.com>
2022-06-13 12:33:09 +0530

    Updates

---
+++ precache-manifest.187c5bb6788e6a37258d146cf425e3f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41c2baca785e059e7a588fa15b767eee",
+    "url": "/TextUtils/index.html"
+  },
+  {
+    "revision": "d45cf9ded819e8b32172",
+    "url": "/TextUtils/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b57ead17ae327e48862a",
+    "url": "/TextUtils/static/js/2.0e216562.chunk.js"
+  },
+  {
+    "revision": "a90bb751b102ed9d8f9443a23ededd32",
+    "url": "/TextUtils/static/js/2.0e216562.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e44866d3c955f61271a",
+    "url": "/TextUtils/static/js/3.bf7aff4a.chunk.js"
+  },
+  {
+    "revision": "d45cf9ded819e8b32172",
+    "url": "/TextUtils/static/js/main.a1510a89.chunk.js"
+  },
+  {
+    "revision": "775e9da76ad0931930f4",
+    "url": "/TextUtils/static/js/runtime-main.bd64c5c0.js"
+  }
+]);
